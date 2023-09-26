17e580f99ef751a746fddd9a4558ab4843b8be71
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2023-09-26 22:52:48 +0300

    feat:(added Button compont)

---
+++ src/ui/Button.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/prop-types */
+export default function Button({ children, handleClick }) {
+  return (
+    <button
+      onClick={() => handleClick()}
+      className="shawdow-emerald-500/50 rounded-full border border-emerald-400  px-4 pb-1 pt-2 font-bold uppercase leading-none text-emerald-800 shadow-lg transition-all duration-200 hover:scale-105 sm:text-xl"
+    >
+      {children}
+    </button>
+  );
+}
