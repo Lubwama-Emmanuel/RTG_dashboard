6e20ef275440d1e5cdf72b6ab6ed40d27492b327
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2023-09-26 23:21:57 +0300

    feat:(added form data)

---
+++ Data.js
@@ -0,0 +1,14 @@
+export const dellProducts = ["xps", "precision"];
+
+export const hpProducts = ["elitebook", "spectre", "probook", "omen"];
+
+export const brands = ["dell", "hp", "lenovo", "acer"];
+
+export const cores = ["i3", "i5", "i7", "i9"];
+
+export const generations = ["4th", "5th", "6th", "7th"];
+
+export const ryzens = [5, 7];
+
+export const ssd = ["128gb", "256gb", "512gb", "1tb"];
+export const hdd = ["500gb", "1tb"];
