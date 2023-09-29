d7798a523fc20b219ec5256057d93176c10cdb3b
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2023-09-29 21:55:37 +0300

    feat:(made the components more  responsive)

--- src/ui/DropDown.jsx
+++ src/ui/DropDown.jsx
@@ -1,9 +1,11 @@
+import Label from "./Label";
+
 /* eslint-disable react/prop-types */
 export default function DropDown({ selectedValue, onChange, label, options }) {
   return (
     <div className="flex flex-col items-start">
-      <label>{label}</label>
-      <select value={selectedValue} onChange={onChange}>
+      <Label>{label}</Label>
+      <select className="capitalize" value={selectedValue} onChange={onChange}>
         {options.map((el) => (
           <option value={el} key={Math.random()}>
             {el}
---
+++ src/ui/Label.jsx
@@ -0,0 +1,4 @@
+/* eslint-disable react/prop-types */
+export default function Label({ children }) {
+  return <label className="text-xl font-medium capitalize">{children}</label>;
+}
