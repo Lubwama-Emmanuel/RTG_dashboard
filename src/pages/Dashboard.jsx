9671ab98ea39cc265d1a5e63a6678c782e0aae02
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2023-09-29 21:54:55 +0300

    feat:(added more form elements)

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import DropDown from "../ui/DropDown";
-import Button from "../ui/Button";
+// import Button from "../ui/Button";
 import axios from "axios";
 
 import {
@@ -13,32 +13,49 @@ import {
   ssd,
   hdd,
 } from "../../Data";
+import Label from "../ui/Label";
 
 const baseUrl = `http://127.0.0.1:9000/api/v1`;
 
 export default function Dashboard() {
   const [name, setName] = useState("");
-  const [brand, setBrand] = useState("");
+  const [brand, setBrand] = useState("dell");
   const [productLine, setProductLine] = useState("");
   const [core, setCore] = useState("");
   const [generation, setGeneration] = useState("");
   const [processor, setProcessor] = useState("");
   const [storage, setStorage] = useState("ssd");
   const [size, setSize] = useState("");
-  const [image, setImage] = useState(null);
+  const [image, setImage] = useState("");
+  const [images, setImages] = useState([]);
+  const [screen, setScreen] = useState("");
+  const [price, setPrice] = useState("");
+  const [desc, setDesc] = useState("");
 
   async function handleSubmit(e) {
     e.preventDefault();
 
     const formData = new FormData();
-    formData.append("image", image);
+    // formData.append("images", images);
+
+    formData.append("images", image);
+
+    images.map((img) => {
+      formData.append("images", img);
+    });
+
     formData.append("name", name);
     formData.append("brand", brand);
     formData.append("processor", processor);
     formData.append("storage", storage);
     formData.append("size", size);
     formData.append("generation", generation);
+    formData.append("screen", screen);
+    formData.append("price", price);
     formData.append("core", core);
+    formData.append("desc", desc);
+
+    console.log(formData);
 
     try {
       await axios.post(`${baseUrl}/laptops/addLaptop`, formData);
@@ -46,35 +63,50 @@ export default function Dashboard() {
       return;
     }
 
-    setName("");
-    setBrand("");
-    setCore("");
-    setGeneration("");
-    setProcessor("");
-    setProcessor("");
-    setProductLine("");
-    setSize("");
-    setImage(null);
-    setStorage("");
+    // setName("");
+    // setBrand("");
+    // setCore("");
+    // setGeneration("");
+    // setProcessor("");
+    // setProcessor("");
+    // setProductLine("");
+    // setSize("");
+    // setStorage("");
+  }
+
+  function handleImagesUpload(e) {
+    const files = Array.from(e.target.files);
+    setImages(files);
   }
 
   return (
     <div className="">
       <h2 className="mb-5 text-3xl">Welcome, Rex!</h2>
       <form onSubmit={handleSubmit} className="space-y-2">
-        <div className="flex flex-col items-start">
-          <label>Laptop Name</label>
+        <div className="flex gap-5">
+          <Label>Laptop Name</Label>
           <input
             type="text"
             placeholder="Enter laptop name"
             value={name}
             onChange={(e) => setName(e.target.value)}
-            className="rounded-lg focus:outline-none focus:ring focus:ring-emerald-600 focus:ring-offset-2"
+            className="w-56 rounded-lg border border-emerald-500 px-2 py-1 focus:outline-none focus:ring focus:ring-emerald-600 focus:ring-offset-2"
           />
         </div>
-        <div className="flex flex-col items-start">
-          <label>Laptop Image</label>
-          <input type="file" onChange={(e) => setImage(e.target.files[0])} />
+        <div className="flex">
+          <div>
+            <Label>Main Image</Label>
+            <input type="file" onChange={(e) => setImage(e.target.files[0])} />
+          </div>
+          <div>
+            <Label>other images</Label>
+            <input
+              type="file"
+              accept="image/*"
+              multiple
+              onChange={(e) => handleImagesUpload(e)}
+            />
+          </div>
         </div>
         <div className="flex">
           <DropDown
@@ -100,7 +132,7 @@ export default function Dashboard() {
             />
           )}
         </div>
-        <div className="flex">
+        <div className="flex gap-5">
           <DropDown
             selectedValue={processor}
             label="processor"
@@ -108,7 +140,7 @@ export default function Dashboard() {
             onChange={(e) => setProcessor(e.target.value)}
           />
           {processor === "intel" && (
-            <div className="flex">
+            <div className="flex gap-5">
               <DropDown
                 selectedValue={core}
                 onChange={(e) => setCore(e.target.value)}
@@ -132,7 +164,7 @@ export default function Dashboard() {
             />
           )}
         </div>
-        <div className="flex">
+        <div className="flex gap-5">
           <DropDown
             selectedValue={storage}
             label="storage"
@@ -158,17 +190,48 @@ export default function Dashboard() {
             />
           )}
         </div>
-        <div className="flex flex-col items-start">
-          <label>Description</label>
+
+        <div className="flex gap-5">
+          <DropDown
+            selectedValue={screen}
+            onChange={(e) => setScreen(e.target.value)}
+            label="Screen"
+            options={["4k", "1080p"]}
+          />
+          <DropDown
+            selectedValue={screen}
+            onChange={(e) => setScreen(e.target.value)}
+            label="Type"
+            options={["touch", "non-touch"]}
+          />
+          <DropDown
+            selectedValue={screen}
+            onChange={(e) => setScreen(e.target.value)}
+            label="Size"
+            options={["13", "14"]}
+          />
+        </div>
+        <div className="flex gap-5">
+          <Label>{"Price"}</Label>
+          <input
+            type="number"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+            placeholder="Enter price"
+            className="w-56 rounded-lg border border-emerald-500 px-2 py-1 focus:outline-none focus:ring focus:ring-emerald-600 focus:ring-offset-2"
+          />
+        </div>
+        <div className="flex gap-5">
+          <Label>Description</Label>
           <input
             type="text"
             placeholder="Enter laptop description"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            className="rounded-lg focus:outline-none focus:ring focus:ring-emerald-600 focus:ring-offset-2"
+            value={desc}
+            onChange={(e) => setDesc(e.target.value)}
+            className="w-56 rounded-lg border border-emerald-500 px-2 py-1 focus:outline-none focus:ring focus:ring-emerald-600 focus:ring-offset-2"
           />
         </div>
-        <Button>{"submit"}</Button>
+        <button>submit</button>
       </form>
     </div>
   );
