49f0411111388820fa99d647a7e011b85fb53fac
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2023-11-04 22:09:29 +0300

    feat:(added react query)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,21 @@
-import Form from "../components/Form";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Home() {
+  const { data } = useQuery({
+    queryKey: ["laptops"],
+    queryFn: async () => {
+      const { data } = await axios.get(
+        "http://127.0.0.1:9000/api/v1/laptops/getLaptops",
+      );
+      return data;
+    },
+  });
+
   return (
     <div>
-      <Form />
+      <h2>Statistics</h2>
+      <h2>{data.data.length}</h2>
     </div>
   );
 }
