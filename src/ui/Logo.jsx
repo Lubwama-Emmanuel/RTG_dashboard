8aaff306531d4163067f23f3fb26d994ba7ef39c
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2023-11-21 20:15:58 +0300

    feat:added applayout

--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom";
 
+// Reusble logo
 export default function Logo() {
   return (
     <h2 className="bg-gradient-to-r from-stone-100 to-stone-400 bg-clip-text font-logo text-2xl font-semibold text-transparent sm:text-3xl ">
